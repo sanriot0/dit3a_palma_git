a9692b8ce981d338d676b37340c8d0888ecd3f33
STUDENT 17 <sandrapalma050gmail.com>
2025-11-05 11:22:34 +0800

    first commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-app>
+     <v-navigation-drawer
+        v-model="drawer"
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://i.pinimg.com/736x/70/47/f8/7047f8a9d9f8d4faf2d44b11a7d679b7.jpg"
+            subtitle="DIT-3A"
+            title="Sandra Palma"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon @click.stop="drawer =! drawer"></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
+
+    <v-mian>
+        <v-container>
+            <slot/>
+        </v-container>
+    </v-mian>
+
+    </v-app>
+
+  </div>
+</template>
+
+<script setup>
+    const drawer=ref(true)
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>good morning, sir. i am very sorry my strapi wont run because it said</h1>   
+    </div>
+</template>
