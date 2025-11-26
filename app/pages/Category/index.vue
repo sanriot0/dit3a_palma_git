e0aa9e9637819858850ec3e9d292735dd7206b4f
STUDENT 17 <sandrapalma050gmail.com>
2025-11-26 11:59:55 +0800

    add data table

--- app/pages/Category/index.vue
+++ app/pages/Category/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card
-    title="Nutrition"
+    title="Category"
     flat
   >
     <template v-slot:text>
@@ -16,109 +16,30 @@
 
     <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="category.data"
       :search="search"
     ></v-data-table>
   </v-card>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  // import { ref } from 'vue'
+
+  const { data: category,  } = await useFetch('http://localhost:1337/api/caters');
+
 
   const search = ref('')
   const headers = [
-    {
-      align: 'start',
-      key: 'name',
-      sortable: false,
-      title: 'Dessert (100g serving)',
-    },
-    { key: 'calories', title: 'Calories' },
-    { key: 'fat', title: 'Fat (g)' },
-    { key: 'carbs', title: 'Carbs (g)' },
-    { key: 'protein', title: 'Protein (g)' },
-    { key: 'iron', title: 'Iron (%)' },
+    
+    { key: 'category_name', title: 'Category' },
+    { key: 'description', title: 'Description' },
+    { key: 'createdAt', title: 'Date Created' },
+    // { key: 'protein', title: 'Protein (g)' },
+    // { key: 'iron', title: 'Iron (%)' },
   ]
   const desserts = [
     {
-      name: 'Frozen Yogurt',
-      calories: 159,
-      fat: 6,
-      carbs: 24,
-      protein: 4,
-      iron: 1,
-    },
-    {
-      name: 'Ice cream sandwich',
-      calories: 237,
-      fat: 9,
-      carbs: 37,
-      protein: 4.3,
-      iron: 1,
-    },
-    {
-      name: 'Eclair',
-      calories: 262,
-      fat: 16,
-      carbs: 23,
-      protein: 6,
-      iron: 7,
-    },
-    {
-      name: 'Cupcake',
-      calories: 305,
-      fat: 3.7,
-      carbs: 67,
-      protein: 4.3,
-      iron: 8,
-    },
-    {
-      name: 'Gingerbread',
-      calories: 356,
-      fat: 16,
-      carbs: 49,
-      protein: 3.9,
-      iron: 16,
-    },
-    {
-      name: 'Jelly bean',
-      calories: 375,
-      fat: 0,
-      carbs: 94,
-      protein: 0,
-      iron: 0,
-    },
-    {
-      name: 'Lollipop',
-      calories: 392,
-      fat: 0.2,
-      carbs: 98,
-      protein: 0,
-      iron: 2,
-    },
-    {
-      name: 'Honeycomb',
-      calories: 408,
-      fat: 3.2,
-      carbs: 87,
-      protein: 6.5,
-      iron: 45,
-    },
-    {
-      name: 'Donut',
-      calories: 452,
-      fat: 25,
-      carbs: 51,
-      protein: 4.9,
-      iron: 22,
-    },
-    {
-      name: 'KitKat',
-      calories: 518,
-      fat: 26,
-      carbs: 65,
-      protein: 7,
-      iron: 6,
-    },
+
+    }
   ]
 </script>
--- app/pages/Inventory/index.vue
+++ app/pages/Inventory/index.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>
-        <h1>Inventory ngani</h1>
-    </div>
+  <v-card
+    title="Inventory"
+    flat
+  >
+    <template v-slot:text>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        hide-details
+        single-line
+      ></v-text-field>
+    </template>
+
+    <v-data-table
+      :headers="headers"
+      :items="inventory.data"
+      :search="search"
+    ></v-data-table>
+  </v-card>
 </template>
+
+<script setup>
+  // import { ref } from 'vue'
+
+  const { data: inventory,  } = await useFetch('http://localhost:1337/api/inventories');
+
+
+  const search = ref('')
+  const headers = [
+    
+    { key: 'product_name', title: 'Product Name' },
+    { key: 'product_description', title: 'Product Description' },
+    { key: 'quantity', title: 'Quantity' },
+    { key: 'condition', title: 'Condition' },
+    { key: 'location', title: 'Location' },
+  ]
+  const desserts = [
+    {
+
+    }
+  ]
+</script>
